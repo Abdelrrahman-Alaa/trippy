fe1902ef3590071a8ef0c9ca3e93266b238a2ada
Abdelrahman Alaa <Abdelrrahman.Alaa@gmail.com>
2026-04-30 06:30:26 +0300

    Refactor: Remove unused components and styles
    - Deleted City, CountryItem, User, and Form components along with their associated CSS files.
    - Removed Spinner and SpinnerFullPage components and their styles.
    - Cleaned up CityList, CountryList, and Sidebar styles.
    - Introduced new components: City, CountryItem, Form, Spinner, and User with updated styles.
    - Added new navigation components: PageNav and Logo with corresponding styles.
    - Implemented CSS Modules for new components to maintain styling consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import Products from "./pages/Products";
+import Products from "./pages/Product";
 import About from "./pages/About";
 import Pricing from "./pages/Pricing";
 import NotFound from "./pages/NotFound";
-import Homepage from './pages/Homepage';
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <PageNav /> */}
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Products />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
